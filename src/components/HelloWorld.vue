9bd8d6b10ff06f7e059208a12070b52f45978636
madhavi <m.borkar1@zensar.com>
2022-05-27 12:00:19 +0530

    vue assigment code added

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+ <template>
+  <div>
+    <h2>Filter LearnVue Articles</h2>
+    <input type="text" placeholder="Filter Search" v-model="query" />
+    <br />
+    <button v-on:click="reset()">Reset</button>
+    <search-results v-bind:query='query'/>
+
+  </div>
+</template>
+
+
+<script>
+    
+import { ref } from "vue";
+import SearchResults from './TitleResults.vue'
+
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    components: {
+      SearchResults
+    },
+    setup() {
+    const query = ref("");
+
+    const reset = () => {
+      query.value = "";
+    };
+
+    return {
+      reset,
+      query,
+    };
+  },
+};
+
+  
+  
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+input {
+    margin-bottom: 24px;
+    border: 2px solid darkcyan;
+    padding: 10px;
+    border-radius: 3px;
+    outline: none;
+}
+
+button {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/TitleResults.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="root">
+        <p class="filter-item-title-number">Showing {{ filteredTitles.length }} results for "{{ query }}"</p>
+        <ul class="filter-list-section ">
+            <li v-for="title in filteredTitles" v-bind:key="title.Page" class="filter-list-item ">
+                {{ title.Name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+    props: {
+        query: String,
+    },
+    setup(props) {
+        var titles = [
+            {
+                Name: "Do something nice for someone I care about",
+               
+            },
+            {
+                Name: "Contribute code or a monetary donation to an open-source software project",
+                
+            },
+            {
+                Name: "Take a class at local community center that interests you",
+                
+            },
+             {
+                Name: "Whatever the mind of man can conceive and believe, it can achieve",
+               
+            },
+            {
+                Name: "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference",
+                
+            },
+            {
+                Name: "I attribute my success to this: I never gave or took any excuse",
+                
+            },
+             {
+                Name: "We must balance conspicuous consumption with conscious capitalism.",
+               
+            }
+        ]
+        const filteredTitles = computed(() => {
+            return titles.filter((s) =>
+                s.Name.toLowerCase().includes(props.query.toLowerCase())
+            );
+        });
+        return {
+            filteredTitles,
+        };
+    },
+};
+</script>
+
+<style>
+.root{
+   
+    width: 40%;
+    margin: auto;
+
+}
+.filter-list-item {
+     padding: 15px;
+    border: 1px solid #ccc;
+    border-bottom: none; 
+    list-style-type:none;
+}
+.filter-list-section {
+    padding-top: 20px;
+    height: 300px;
+    overflow-y: scroll;
+}
+.filter-item-title-number {
+    text-align: right;
+}
+.filter-list-item:last-child {
+    border-bottom: 1px solid #ccc;
+}
+
+</style>
